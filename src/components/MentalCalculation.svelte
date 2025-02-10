0f1d5e07f31f04e954b536dbbc4e1aa6cf2c4c9a
botlane-feeder <tserieye.pro@gmail.com>
2025-02-10 17:34:02 +0100

    UPDATE : Auto lancement d'une nouvelle equation tant que le countdownTime n'est pas à 0

--- src/components/MentalCalculation.svelte
+++ src/components/MentalCalculation.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
   import type { EquationType } from "$lib/types";
   
+  import CountdownTime from "./CountdownTime.svelte";
   import Equation from "./Equation.svelte";
-  import Countdown from "./Countdown.svelte";
+  import CountdownBar from "./CountdownBar.svelte";
   import Keypad from "./Keypad.svelte";
 
   let { equationArray }:{ equationArray:EquationType[] } = $props();
 
   let response:number = $state(0);
-  let winAnimating=$state(false);
-  let wrongAnimating=$state(false);
+  let winAnimating:boolean=$state(false);
+  let wrongAnimating:boolean=$state(false);
   let idEquation:number = $state( sortEquation( equationArray.length-1 ) );
 
-  let startCountdown= $state(false);
-  let pauseCountdown= $state(false);
-  let resetCountdown= $state(false);
-  let timerEnding= $state(false);
+  let startCountdownTime:boolean=$state(false)
+  let countdownTimeEnd:boolean= $state(false);
+
+  let startCountdown:boolean= $state(false);
+  let pauseCountdown:boolean= $state(false);
+  let resetCountdown:boolean= $state(false);
+  let countdownBarEnd:boolean= $state(false);
 
   let equation:string = $derived(equationArray[idEquation]["equation"]);
 
@@ -24,10 +28,8 @@
     if(equationArray[idEquation]["result"] == response){
       // Affichage vert
       winAnimation();
-      // Nouvelle équation
-      idEquation = sortEquation( equationArray.length-1 );
-      // Relance du chrono
-      startCountdown=true;
+      // Lance une nouvelle équation
+      startNewEquation()
     }else{
       // Affichage rouge
       wrongAnimation();
@@ -35,6 +37,13 @@
   }
   function sortEquation(max:number):number{
     return Math.floor(Math.random() * max);
+  }
+  function startNewEquation(){
+    // Selectionne une nouvelle equation
+    idEquation = sortEquation( equationArray.length-1 );
+    // Relance du chrono
+    startCountdown=true;
+
   }
 
   function winAnimation() {
@@ -49,16 +58,23 @@
       wrongAnimating = false;
     }, 1000);
   }
+  $effect(()=>{
+    if(!countdownTimeEnd && countdownBarEnd){
+      startNewEquation();
+    }
+  })
 </script>
 
 <div class="container">
-  {#if timerEnding}
+  {#if countdownTimeEnd}
     <div>FIN, Score : </div>
   {/if}
+  <CountdownTime bind:startTrigger={startCountdownTime} bind:end={countdownTimeEnd} duration={2}/>
+  <button onclick={()=>{startCountdownTime=true}}>Start</button>
   {#key winAnimating || wrongAnimating}
     <Equation {equation} {response} {winAnimating} {wrongAnimating}/>
   {/key}
-  <Countdown bind:startTrigger={startCountdown} bind:pauseTrigger={pauseCountdown} bind:resetTrigger={resetCountdown} duration={3000} bind:end={timerEnding}/>
+  <CountdownBar bind:startTrigger={startCountdown} bind:pauseTrigger={pauseCountdown} bind:resetTrigger={resetCountdown} duration={3000} bind:end={countdownBarEnd}/>
   <Keypad bind:response={response} {verification}/>
 </div>
 
