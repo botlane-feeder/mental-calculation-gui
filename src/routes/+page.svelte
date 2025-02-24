a7495b9198c0ba408a9180108f71360f9f9c8de8
botlane-feeder <tserieye.pro@gmail.com>
2025-02-24 21:28:19 +0100

    ADD : Ajout d'un stockage de score dans le navigateur

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,82 @@
   import MentalCalculation from "../components/MentalCalculation.svelte";
   import Navbar from "../components/Navbar.svelte";
   import type { EquationType } from "$lib/types";
+  import { onMount } from "svelte";
+  import Modal from "../components/Modal.svelte";
 
+  interface Profile{
+    name:string,
+    level:number,
+    scoreArray:number[]
+  }
+
+  
   let { data }= $props();
+  let localProfile:Profile=$state({name:"", scoreArray:[], level:0});
+  //TODO : faire calculer le score à l'intérieur !
+  let score:number=$state(0);
 
-  let newGameTrigger:boolean=$state(false);
+  // let newGameTrigger:boolean=$state(false);
+  let gameTrigger:{new:boolean, end:boolean}=$state({new:false, end:true});
   let level:number = $state(0);
   let levelString:string[]=["facile", "moyen", "difficile"];
   let equationArray:EquationType[] = $derived(data["equationArray"][level]);
+
+  let mentalCalculatioSettings = {
+    globalTimer:10
+  };
+
+  // Quand l'application est disponible sur navigateur
+  onMount(()=>{
+    // Récupération du profil local
+    updateProfile();
+  });
+  function updateProfile(){
+    let storage = localStorage.getItem("mental-calculation");
+    if(storage!=null) localProfile = JSON.parse(storage);
+  }
+  $effect(()=>{
+    saveProfile(localProfile);
+  })
+  function saveProfile(localProfile:Profile){
+    localStorage.setItem("mental-calculation", JSON.stringify(localProfile));
+  } 
+  function addScore(newScore:number){
+    // let scores = [3,2,5,6]//localStorage.getItem("mental-calculation")["scores"];
+    let scoreArray:number[] = localProfile["scoreArray"]
+    scoreArray.push(newScore);
+    scoreArray.sort((a,b)=>b-a)
+    // Limite à 3 scores
+    if(scoreArray.length > 2) scoreArray.splice(3);
+    // Enregistre
+    console.log(scoreArray);
+    localProfile["scoreArray"] = scoreArray;
+    // localStorage.setItem("mental-calculation", JSON.stringify(localProfile));
+  }
+  // $effect(()=>{
+  //   if( gameTrigger["end"] == true ){
+  //     addScore(score);
+  //   }
+  // });
 </script>
 
 <div class="container">
-  <Navbar bind:newGameTrigger={newGameTrigger} bind:level={level} height={"50px"}/>
+  <Navbar bind:newGameTrigger={gameTrigger["new"]} bind:level={localProfile["level"]} height={"50px"}/>
   <div class="main">
-    <MentalCalculation bind:newGameTrigger={newGameTrigger} { equationArray } level={levelString[level]}/>
+    <MentalCalculation bind:gameTrigger={gameTrigger} { equationArray } {mentalCalculatioSettings} {score}/>
   </div>
 </div>
+<Modal onclick={()=>{ gameTrigger["new"] = true}} title={`Résolvez autant d'équation possible en ${mentalCalculatioSettings["globalTimer"]} s`} show={gameTrigger["end"]}>
+  <!-- {#if score > 0}
+    <div class="score">Au niveau <span class="bold">{levelString[level]}</span>, votre score est de {score} !</div>
+  {:else}
+    <div class="score">Obtenez votre score après votre tentative</div>
+  {/if} -->
+  {#if localProfile}
+    Améliorez votre score {localProfile["scoreArray"]}
+  {/if}
+</Modal>
+<!-- <Modal></Modal> -->
 
 
 <style>
